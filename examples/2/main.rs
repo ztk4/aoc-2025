f534fbf622977b7772c66c88428e6795b0af466a
Zach Kaplan <zacharytrent@gmail.com>
2025-12-17 22:55:20 -0800

    Solved Day 2 Part 2
    Did you know about work group functions??
    
    These are very convenient compared to manual aggregation within a group, and apparently optimized on some hardware

--- examples/2/main.rs
+++ examples/2/main.rs
@@ -57,8 +57,8 @@ fn sum(
 /// Represents a (closed) range of ID values.
 #[derive(Debug)]
 struct IdRange {
-  lo: u64,
-  hi: u64,
+  lo: usize,
+  hi: usize,
 }
 
 impl IdRange {
@@ -66,13 +66,13 @@ impl IdRange {
     let (lo, hi) = input
       .as_ref()
       .split('-')
-      .map(str::parse::<u64>)
+      .map(str::parse::<usize>)
       .collect_tuple()
       .ok_or_else(|| eyre!("Failed to parse {} as range", input.as_ref()))?;
     Ok(IdRange { lo: lo?, hi: hi? })
   }
 
-  fn len(&self) -> u64 {
+  fn len(&self) -> usize {
     (self.hi - self.lo) + 1
   }
 
@@ -106,6 +106,41 @@ impl IdRange {
     }
 
     sum(&partial_sums, ctx, dev, prog, lws)
+  }
+
+  fn sum_invalid_extended(
+    &self,
+    ctx: &Context,
+    dev: Device,
+    prog: &Program,
+    lws: usize,
+  ) -> Result<u64> {
+    let queue = Queue::new(ctx, dev, None)?;
+    let invalid = Buffer::<u64>::builder()
+      .queue(queue.clone())
+      .len(self.len())
+      .fill_val(0)
+      .build()?;
+
+    // All inputs are <16 digits long, so the largest chunks are half that.
+    const MAX_CHUNK_SIZE: usize = 8;
+    let get_invalid = Kernel::builder()
+      .program(&prog)
+      .name("get_invalid_extended")
+      .queue(queue.clone())
+      .global_work_size((self.len(), MAX_CHUNK_SIZE))
+      .global_work_offset((0, 1)) // start with chunks of size 1 (not 0).
+      .local_work_size((1, MAX_CHUNK_SIZE))
+      .arg(self.lo)
+      .arg(&invalid)
+      .build()?;
+
+    unsafe {
+      get_invalid.enq()?;
+      debug!("{:?}", buf2vec(&invalid)?);
+    }
+
+    sum(&invalid, ctx, dev, prog, lws)
   }
 }
 
@@ -147,7 +182,10 @@ fn main() -> Result<()> {
 
   let invalid_sums: Vec<_> = ranges
     .into_iter()
-    .map(|range| range.sum_invalid(&context, device, &program, lws))
+    .map(|range| match config.part {
+      Part::One => range.sum_invalid(&context, device, &program, lws),
+      Part::Two => range.sum_invalid_extended(&context, device, &program, lws),
+    })
     .collect::<Result<_>>()?;
   info!("Invalid sums: {invalid_sums:?}");
 
--- examples/2/product_ids.cl
+++ examples/2/product_ids.cl
@@ -25,6 +25,37 @@ __kernel void sum_invalid(ulong start, ulong end, __global ulong* sums,
   }
 }
 
+// True if an ID is invalid considering chunks of n digits.
+bool invalid_for_chunk(ulong id, size_t n) {
+  ulong power = 1;
+  while (n-- > 0) power *= 10;
+
+  ulong chunk = id % power;
+  // Invalid chunks can't have leading 0s.
+  if (chunk < power / 10) return false;
+
+  do {
+    id /= power;
+    if (id % power != chunk) return false;
+  } while (id > power);
+
+  return true;
+}
+
+// Generates invalid IDs per the extended definition.
+// NOTE: I originally wanted to do a 2D kernel here (sum reduce on dim 0,
+// boolean or reduce on dim 1), BUT that prevents me from using `work_group_any`
+// which feels like a more efficient paradigm. In this case, that's at the cost
+// of needing a much larger buffer for this step... but maybe that's OK?
+__kernel void get_invalid_extended(ulong start, __global ulong* invalid) {
+  ulong id = start + get_global_id(0);
+
+  bool is_invalid = work_group_any(invalid_for_chunk(id, get_global_id(1)));
+  if (!get_local_id(1) && is_invalid) {
+    invalid[get_global_id(0)] = id;
+  }
+}
+
 __kernel void sum(__global ulong* input, ulong length, __global ulong* sums,
                   __local ulong* scratch) {
   scratch[get_local_id(0)] =

90d72828bab2adeec7a68b6022d63e2fda5d1468
Zach Kaplan <zacharytrent@gmail.com>
2026-02-07 01:03:49 -0800

    WIP: AoC Day 5 Part 2
    Implemented parallel sort for the interval endpoints
    
    Next is a reduction using cumulative sums to track interior vs. exterior

--- examples/5/cafe.cl
+++ examples/5/cafe.cl
@@ -8,17 +8,17 @@
 // NB:
 //   * get_work_dim() == 1
 //   * length(counts) >= get_num_groups(0)
-kernel void count_in_range_gs(global const ulong* ids, int n_ids,
-                              global const ulong2* ranges, int n_ranges,
-                              global ulong* counts) {
-  ulong count = 0;
+kernel void count_in_range_gs(global const long* ids, int n_ids,
+                              global const long2* ranges, int n_ranges,
+                              global long* counts) {
+  long count = 0;
   for (int idx = get_group_id(0); idx < n_ids; idx += get_num_groups(0)) {
-    ulong id = ids[idx];
+    long id = ids[idx];
 
     bool within = false;
     for (int ridx = get_local_id(0); ridx < n_ranges;
          ridx += get_local_size(0)) {
-      ulong2 range = ranges[ridx];
+      long2 range = ranges[ridx];
       within = within || (id >= range.x && id <= range.y);
     }
 
@@ -32,7 +32,61 @@ kernel void count_in_range_gs(global const ulong* ids, int n_ids,
 
 // Part 2: Single group sum reduce.
 // NB: get_local_size(0) == length(counts).
-kernel void reduce_counts(global const ulong* counts, global ulong* result) {
-  ulong res = work_group_reduce_add(counts[get_local_id(0)]);
+kernel void reduce_counts(global const long* counts, global long* result) {
+  long res = work_group_reduce_add(counts[get_local_id(0)]);
   if (!get_local_id(0)) *result = res;
+}
+
+/// Bitonic Merge Sort ///
+
+// Merge: Takes a sequence of bitonic sequences of length k, and merges both
+// halves of each into a monotonic sequence
+// NOTE: The resulting sorted sequences will alternate in sort direction:
+//       first increasing, then descreasing, etc.
+// NOTE: Sequence will be sorted by x; y is assumed to be auxilliary data.
+// NOTE: `size` gives the length of scratch := the number of values processed by
+//       each work group.
+// NB:
+//  * get_local_size(0) MUST be a power of 2 AND <= (size/2).
+//  * k MUST be a power of 2
+//  * Each aligned, consecutive sequence of length k must ALREADY be bitonic.
+//  * Scratch length `size` must be a multiple of k.
+kernel void bitonic_merge(global long2* sequences, const int k,
+                          local long2* scratch, const int size) {
+  if (k <= 1) return;
+
+  for (int2 idx = (int)get_local_id(0) + (int2)(0, get_group_id(0) * size);
+       idx.x < size; idx += (int)get_local_size(0)) {
+    scratch[idx.x] = sequences[idx.y];
+  }
+
+  // Whether the first set of k in size should merge ascending or descending.
+  bool asc0 = !(get_group_id(0) & 1);
+  for (int c = k >> 1; c > 0; c >>= 1) {
+    // Ensure all writes to scratch are committed.
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    // We want indices that map to the lower halves of each pair of sequences.
+    // E.g. c=2 -> 0, 1, 4, 5, 8, 9, ...; c=4 -> 0, 1, 2, 3, 8, 9, 10, 11, ...
+    for (int base = get_local_id(0); base < (size >> 1);
+         base += get_local_size(0)) {
+      // Maps `base` on [0, size >> 1) onto [0, size) as the lower halves.
+      // Does this by always unsetting `c`, and adding on `lws` in that case.
+      int idx = (base & ~c) + (base & c ? get_local_size(0) : 0);
+      bool asc = asc0 ^ !!(idx & k);  // Update with parity over sets of k.
+      long4 pair = (long4)(scratch[idx], scratch[idx + c]);
+      // printf("[%ld, %ld] ^%dv => [%ld, %ld]\n", pair.x, pair.z, asc,
+      //        (pair.x > pair.z) ^ asc ? pair.x : pair.z,
+      //        (pair.x > pair.z) ^ asc ? pair.z : pair.x);
+      pair = (pair.x > pair.z) ^ asc ? pair.xyzw : pair.zwxy;
+      scratch[idx] = pair.xy;
+      scratch[idx + c] = pair.zw;
+    }
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+  for (int2 idx = (int)get_local_id(0) + (int2)(0, get_group_id(0) * size);
+       idx.x < size; idx += (int)get_local_size(0)) {
+    sequences[idx.y] = scratch[idx.x];
+  }
 }
--- examples/5/main.rs
+++ examples/5/main.rs
@@ -1,6 +1,6 @@
 //! AoC Day 5
 #![feature(result_flattening, macro_metavar_expr)]
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Result, eyre};
 use libaoc::*;
 use log::*;
 use ocl::*;
@@ -9,17 +9,58 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 struct Range {
-  lo: u64,
-  hi: u64,
+  lo: i64,
+  hi: i64,
 }
 
 impl FromStr for Range {
   type Err = color_eyre::eyre::Report;
 
   fn from_str(src: &str) -> Result<Self, Self::Err> {
-    let (lo, hi) = tuple_parse_tokens!(DelimitedTokens::by_sep(src, "-") => (u64, u64))?;
+    let (lo, hi) = tuple_parse_tokens!(DelimitedTokens::by_sep(src, "-") => (i64, i64))?;
     Ok(Range { lo, hi })
   }
+}
+
+/// Sorts buffer using bitonic sort.
+/// NB: buffer's length and group_size must be a power of two.
+fn bitonic_sort(buffer: &Buffer<prm::Long2>, proque: &ProQue, group_size: usize) -> Result<()> {
+  if !buffer.len().is_power_of_two() {
+    return Err(eyre!(
+      "Buffer of length {} must be a power of two",
+      buffer.len()
+    ));
+  }
+  if !group_size.is_power_of_two() {
+    return Err(eyre!("Group size {group_size} must be a power of two"));
+  }
+
+  let lws = std::cmp::min(group_size, buffer.len() >> 1);
+  let mut k = 2;
+  let mut kerns: Vec<Kernel> = vec![];
+  while k <= buffer.len() {
+    let scratch_size = std::cmp::max(lws << 1, k);
+    kerns.push(
+      proque
+        .kernel_builder("bitonic_merge")
+        .global_work_size(buffer.len() * lws / scratch_size)
+        .local_work_size(lws)
+        .arg(buffer)
+        .arg(k as i32)
+        .arg_local::<prm::Long2>(scratch_size)
+        .arg(scratch_size as i32)
+        .build()?,
+    );
+
+    k <<= 1;
+  }
+
+  for kern in kerns {
+    unsafe { kern.enq()? }
+    debug!("buffer: {:?}", buf2vec(buffer)?);
+  }
+
+  Ok(())
 }
 
 fn main() -> Result<()> {
@@ -40,7 +81,7 @@ fn main() -> Result<()> {
     str::parse::<Range>,
   )
   .collect::<Result<_>>()?;
-  let ids: Vec<_> = parse_result_lines(lines, str::parse::<u64>).collect::<Result<_>>()?;
+  let ids: Vec<_> = parse_result_lines(lines, str::parse::<i64>).collect::<Result<_>>()?;
   info!("Input Sizes: {}, {}", ranges.len(), ids.len());
   debug!("Input: {:?}, {:?}", ranges, ids);
 
@@ -49,65 +90,90 @@ fn main() -> Result<()> {
     .dims(1)
     .build()?;
 
-  let ids = proque
-    .buffer_builder::<u64>()
-    .len(ids.len())
-    .copy_host_slice(&ids)
-    .build()?;
-  let ranges = proque
-    .buffer_builder::<prm::Ulong2>()
-    .len(ranges.len())
-    .copy_host_slice(
-      &ranges
-        .iter()
-        .map(|r| [r.lo, r.hi].into())
-        .collect::<Vec<_>>(),
-    )
-    .build()?;
+  match config.part {
+    Part::One => {
+      let ids = proque
+        .buffer_builder::<i64>()
+        .len(ids.len())
+        .copy_host_slice(&ids)
+        .build()?;
+      let ranges = proque
+        .buffer_builder::<prm::Long2>()
+        .len(ranges.len())
+        .copy_host_slice(
+          &ranges
+            .iter()
+            .map(|r| [r.lo, r.hi].into())
+            .collect::<Vec<_>>(),
+        )
+        .build()?;
 
-  let gs_lws = config.group_size;
-  let mut get_counts = proque
-    .kernel_builder("count_in_range_gs")
-    .local_work_size(gs_lws)
-    .arg(&ids)
-    .arg(ids.len() as i32)
-    .arg(&ranges)
-    .arg(ranges.len() as i32)
-    .arg_named("counts", None::<&Buffer<u64>>) // We don't know the size yet.
-    .build()?;
+      let gs_lws = config.group_size;
+      let mut get_counts = proque
+        .kernel_builder("count_in_range_gs")
+        .local_work_size(gs_lws)
+        .arg(&ids)
+        .arg(ids.len() as i32)
+        .arg(&ranges)
+        .arg(ranges.len() as i32)
+        .arg_named("counts", None::<&Buffer<i64>>) // We don't know the size yet.
+        .build()?;
 
-  let r_lws = get_mem_bound_reduce_num_groups_hint(&proque.device(), &get_counts)?;
-  let gws =
-    get_mem_bound_reduce_gws_hint(&proque.device(), &get_counts, gs_lws, ids.len() * gs_lws)?;
-  info!("GWS ({gws:?}), Reduce LWS ({r_lws})");
+      let r_lws = get_mem_bound_reduce_num_groups_hint(&proque.device(), &get_counts)?;
+      let gws =
+        get_mem_bound_reduce_gws_hint(&proque.device(), &get_counts, gs_lws, ids.len() * gs_lws)?;
+      info!("GWS ({gws:?}), Reduce LWS ({r_lws})");
 
-  let counts = proque
-    .buffer_builder::<u64>()
-    .len(r_lws)
-    .fill_val(0)
-    .build()?;
-  let result = proque.buffer_builder::<u64>().fill_val(0).build()?;
+      let counts = proque
+        .buffer_builder::<i64>()
+        .len(r_lws)
+        .fill_val(0)
+        .build()?;
+      let result = proque.buffer_builder::<i64>().fill_val(0).build()?;
 
-  get_counts.set_default_global_work_size(gws);
-  get_counts.set_arg("counts", Some(&counts))?;
+      get_counts.set_default_global_work_size(gws);
+      get_counts.set_arg("counts", Some(&counts))?;
 
-  let reduce_counts = proque
-    .kernel_builder("reduce_counts")
-    .global_work_size(r_lws)
-    .local_work_size(r_lws)
-    .arg(&counts)
-    .arg(&result)
-    .build()?;
+      let reduce_counts = proque
+        .kernel_builder("reduce_counts")
+        .global_work_size(r_lws)
+        .local_work_size(r_lws)
+        .arg(&counts)
+        .arg(&result)
+        .build()?;
 
-  unsafe {
-    get_counts.enq()?;
-    reduce_counts.enq()?;
+      unsafe {
+        get_counts.enq()?;
+        reduce_counts.enq()?;
+      }
+
+      println!(
+        "Fresh Count: {}",
+        buf2vec(&result)?.into_iter().exactly_one()?
+      );
+    }
+    Part::Two => {
+      let size = (ranges.len() << 1).next_power_of_two();
+      let endpoints = proque
+        .buffer_builder::<prm::Long2>()
+        .len(size)
+        .copy_host_slice(
+          &ranges
+            .iter()
+            // Using `1` to denote opening endpoints, and `-1` for closing.
+            .map(|r| [r.lo, 1])
+            .chain(ranges.iter().map(|r| [r.hi, -1]))
+            // Sentinel values to pad to power of two.
+            .chain(std::iter::repeat([0, 0]))
+            .take(size)
+            .map_into::<prm::Long2>()
+            .collect::<Vec<_>>(),
+        )
+        .build()?;
+
+      bitonic_sort(&endpoints, &proque, config.group_size)?;
+    }
   }
-
-  println!(
-    "Fresh Count: {}",
-    buf2vec(&result)?.into_iter().exactly_one()?
-  );
 
   Ok(())
 }

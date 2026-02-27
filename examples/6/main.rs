7605b3e444f11fce0ca308a51af2358bb2490fb8
Zach Kaplan <zacharytrent@gmail.com>
2026-02-26 21:17:09 -0800

    Day 6 Part 2
    I just kind of decided the easiest thing to do was give the GPU the entire input and parse numbers there.
    
    This ended up working great LOL
    
    Notably there's some significant duplication across the code, but I think it's reasonable enough for this kind of toy problem

--- examples/6/main.rs
+++ examples/6/main.rs
@@ -10,81 +10,180 @@ fn main() -> Result<()> {
   env_logger::init();
   color_eyre::install()?;
 
-  let config = create_config!()?;
+  let mut config = create_config!()?;
   info!(
     "Advent of Code day #{}, part {:?}!",
     config.day, config.part
   );
-  // Sample of using the parse macro, assuming each line is 3 ints.
-  let input: Vec<_> = parse_result_lines(config.input.lines(), |line| {
-    match line.trim().chars().next().unwrap() {
-      '0'..='9' => vec_parse_tokens::<u64>(DelimitedTokens::by_whitespace(line)),
-      // For the operations, let's just map to code points.
-      '*' | '+' => vec_parse_tokens::<char>(DelimitedTokens::by_whitespace(line))
-        .map(|v| v.into_iter().map(|c| c as u64).collect()),
-      c => Err(eyre!("Unsupported char '{c}'")),
-    }
-  })
-  .collect::<Result<_>>()?;
-  let nprob = input[0].len();
-  info!("Found {} lines encoding {nprob} problems", input.len());
-  debug!("Input: {:?}", input);
 
   let proque = ProQue::builder()
     .src(fs::read_to_string(config.local_dir.join("math.cl"))?)
     .dims(1)
     .build()?;
 
-  let equations = proque
-    .buffer_builder::<u64>()
-    .len(input.len() * nprob)
-    .copy_host_slice(&input.iter().cloned().flatten().collect::<Vec<_>>())
-    .build()?;
+  match config.part {
+    Part::One => {
+      let input: Vec<_> = parse_result_lines(config.input.lines(), |line| {
+        match line.trim().chars().next().unwrap() {
+          '0'..='9' => vec_parse_tokens::<u64>(DelimitedTokens::by_whitespace(line)),
+          // For the operations, let's just map to code points.
+          '*' | '+' => vec_parse_tokens::<char>(DelimitedTokens::by_whitespace(line))
+            .map(|v| v.into_iter().map(|c| c as u64).collect()),
+          c => Err(eyre!("Unsupported char '{c}'")),
+        }
+      })
+      .collect::<Result<_>>()?;
+      let nprob = input[0].len();
+      info!("Found {} lines encoding {nprob} problems", input.len());
+      debug!("Input: {:?}", input);
 
-  let mut sum_equations_partial = proque
-    .kernel_builder("sum_equations_partial_reduce")
-    .local_work_size(config.group_size)
-    .arg(&equations)
-    .arg(nprob)
-    .arg(input.len())
-    .arg_named("partials", None::<&Buffer<u64>>)
-    .build()?;
+      let equations = proque
+        .buffer_builder::<u64>()
+        .len(input.len() * nprob)
+        .copy_host_slice(&input.iter().cloned().flatten().collect::<Vec<_>>())
+        .build()?;
 
-  let ngroups = get_mem_bound_reduce_num_groups_hint(&proque.device(), &sum_equations_partial)?;
-  let gws = get_mem_bound_reduce_gws_hint(
-    &proque.device(),
-    &sum_equations_partial,
-    config.group_size,
-    nprob,
-  )?;
+      let mut sum_equations_partial = proque
+        .kernel_builder("sum_equations_partial_reduce")
+        .local_work_size(config.group_size)
+        .arg(&equations)
+        .arg(nprob)
+        .arg(input.len())
+        .arg_named("partials", None::<&Buffer<u64>>)
+        .build()?;
 
-  let partials = proque
-    .buffer_builder::<u64>()
-    .len(ngroups)
-    .fill_val(0)
-    .build()?;
-  let result = proque.buffer_builder::<u64>().fill_val(0).build()?;
+      let ngroups = get_mem_bound_reduce_num_groups_hint(&proque.device(), &sum_equations_partial)?;
+      let gws = get_mem_bound_reduce_gws_hint(
+        &proque.device(),
+        &sum_equations_partial,
+        config.group_size,
+        nprob,
+      )?;
 
-  sum_equations_partial.set_default_global_work_size(gws);
-  sum_equations_partial.set_arg("partials", &partials)?;
+      let partials = proque
+        .buffer_builder::<u64>()
+        .len(ngroups)
+        .fill_val(0)
+        .build()?;
+      let result = proque.buffer_builder::<u64>().fill_val(0).build()?;
 
-  let sum_full_reduce = proque
-    .kernel_builder("sum_full_reduce")
-    .global_work_size(ngroups)
-    .local_work_size(ngroups)
-    .arg(&partials)
-    .arg(&result)
-    .build()?;
+      sum_equations_partial.set_default_global_work_size(gws);
+      sum_equations_partial.set_arg("partials", &partials)?;
 
-  unsafe {
-    sum_equations_partial.enq()?;
-    debug!("Partials: {:?}", buf2vec(&partials)?);
-    sum_full_reduce.enq()?;
+      let sum_full_reduce = proque
+        .kernel_builder("sum_full_reduce")
+        .global_work_size(ngroups)
+        .local_work_size(ngroups)
+        .arg(&partials)
+        .arg(&result)
+        .build()?;
+
+      unsafe {
+        sum_equations_partial.enq()?;
+        debug!("Partials: {:?}", buf2vec(&partials)?);
+        sum_full_reduce.enq()?;
+      }
+
+      println!(
+        "Equation Sum: {}",
+        buf2vec(&result)?.into_iter().exactly_one()?
+      );
+    }
+    Part::Two => {
+      let mut numchars: Vec<u8> = vec![];
+      let mut operators: Vec<u8> = vec![];
+      loop {
+        let mut line: Vec<u8> = vec![];
+        if config.input.read_until(b'\n', &mut line)? == 0 {
+          break;
+        }
+        match line[0] {
+          b'+' | b'*' => operators = line,
+          _ => numchars.append(&mut line),
+        };
+      }
+      // Assuming every line is the same length for now.
+      let llen = operators.len();
+      let nlines = numchars.len() / llen;
+      info!("Found {} lines", nlines + 1);
+      debug!("Input: {numchars:?}{operators:?}");
+
+      let chars = proque
+        .buffer_builder::<u8>()
+        .len(numchars.len())
+        .copy_host_slice(&numchars)
+        .build()?;
+      let ops = proque
+        .buffer_builder::<u8>()
+        .len(operators.len())
+        .copy_host_slice(&operators)
+        .build()?;
+
+      let numbers = proque
+        .buffer_builder::<u64>()
+        .len(llen)
+        .fill_val(0)
+        .build()?;
+
+      let parse_numbers = proque
+        .kernel_builder("parse_numbers")
+        .global_work_size(llen.next_multiple_of(config.group_size))
+        .local_work_size(config.group_size)
+        .arg(&chars)
+        .arg(nlines)
+        .arg(llen)
+        .arg(&numbers)
+        .build()?;
+
+      let mut sum_equations_partial = proque
+        .kernel_builder("sum_col_equations_partial_reduce")
+        .local_work_size(config.group_size)
+        .arg(&numbers)
+        .arg(&ops)
+        .arg(llen)
+        .arg_named("partials", None::<&Buffer<u64>>)
+        .build()?;
+
+      let ngroups = get_mem_bound_reduce_num_groups_hint(&proque.device(), &sum_equations_partial)?;
+      let gws = get_mem_bound_reduce_gws_hint(
+        &proque.device(),
+        &sum_equations_partial,
+        config.group_size,
+        llen,
+      )?;
+
+      let partials = proque
+        .buffer_builder::<u64>()
+        .len(ngroups)
+        .fill_val(0)
+        .build()?;
+      let result = proque.buffer_builder::<u64>().fill_val(0).build()?;
+
+      sum_equations_partial.set_default_global_work_size(gws);
+      sum_equations_partial.set_arg("partials", &partials)?;
+
+      let sum_full_reduce = proque
+        .kernel_builder("sum_full_reduce")
+        .global_work_size(ngroups)
+        .local_work_size(ngroups)
+        .arg(&partials)
+        .arg(&result)
+        .build()?;
+
+      unsafe {
+        parse_numbers.enq()?;
+        debug!("Numbers: {:?}", buf2vec(&numbers)?);
+        sum_equations_partial.enq()?;
+        debug!("Partials: {:?}", buf2vec(&partials)?);
+        sum_full_reduce.enq()?;
+      }
+
+      println!(
+        "Equation Sum: {}",
+        buf2vec(&result)?.into_iter().exactly_one()?
+      );
+    }
   }
 
-  println!(
-    "Equation Sum: {}",
-    buf2vec(&result)?.into_iter().exactly_one()?
-  );
   Ok(())
 }
--- examples/6/math.cl
+++ examples/6/math.cl
@@ -5,8 +5,7 @@
 // Each `equations[n + p*nprob]` is part `p` of problem `n`.
 // The last part of each equation should be the operation to reduce by.
 // The partial reductions are written to `partials`.
-// NB:
-//   * GWS/LWS <= len(partials) < CL_DEVICE_MAX_WORK_GROUP_SIZE
+// NB: GWS/LWS <= len(partials) < CL_DEVICE_MAX_WORK_GROUP_SIZE
 kernel void sum_equations_partial_reduce(global const ulong* equations,
                                          ulong nprob, ulong lines,
                                          global ulong* partials) {
@@ -38,3 +37,63 @@ kernel void sum_full_reduce(global const ulong* partials,
   long res = work_group_reduce_add(partials[get_global_id(0)]);
   if (!get_global_id(0)) *result = res;
 }
+
+/// Number Parsing -- Converts columnar numbers to ulongs ///
+
+// Converts each column of chars into a single number.
+// Empty columns convert to 0.
+// NB: GWS >= llen.
+kernel void parse_numbers(global const char* chars, ulong nlines, ulong llen,
+                          global ulong* numbers) {
+  if (get_global_id(0) > llen) return;
+
+  ulong number = 0;
+  for (ulong idx = get_global_id(0), l = 0; l < nlines; idx += llen, ++l) {
+    char ch = chars[idx];
+    if (ch >= '0' && ch <= '9') number = 10 * number + (ch - '0');
+  }
+
+  numbers[get_global_id(0)] = number;
+}
+
+/// 2-Part Sum Reduce -- Evaluates (columnar) equations and then reduces ///
+
+// Part 1: Evalute equations and partially reduce results by summation.
+// `numbers` is a flat buffer of numbers, with 0 separating equations.
+// `ops` is a flat buffer of ops, where the idx corresponding to the first
+// number in an equation denotes the operation to perform.
+// The partial reductions are written to `partials`.
+// NB: GWS/LWS <= len(partials) < CL_DEVICE_MAX_WORK_GROUP_SIZE
+kernel void sum_col_equations_partial_reduce(global const ulong* numbers,
+                                             global const char* ops, ulong len,
+                                             global ulong* partials) {
+  ulong sum = 0;
+  for (ulong idx = get_global_id(0); idx < len; idx += get_global_size(0)) {
+    bool add;
+    switch (ops[idx]) {
+      case '+':
+        add = true;
+        break;
+      case '*':
+        add = false;
+        break;
+      default:
+        continue;
+    }
+
+    ulong result = add ? 0 : 1;
+    for (ulong* num = numbers + idx; *num; ++num) {
+      result = add ? result + *num : result * *num;
+    }
+
+    sum += result;
+  }
+
+  sum = work_group_reduce_add(sum);
+  if (!get_local_id(0)) {
+    partials[get_group_id(0)] = sum;
+  }
+}
+
+// Part 2: Single-group sum reduce.
+// See `sum_full_reduce` above.
